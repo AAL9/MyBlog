2a374ec734c90550463050ce873f463b50032d2f
Abdulaziz <a.alluheeb@gmail.com>
2023-07-04 09:27:33 +0300

    switch to prev commit

--- posts_app/tasks.py
+++ posts_app/tasks.py
@@ -4,4 +4,4 @@ from background_task import background
 def my_scheduled_task():
     # Your task code goes here
     print("Executing scheduled task now...")
-    # This function will be executed at the scheduled time
+    # This function will be executed at the scheduled time 
