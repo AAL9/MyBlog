5459280bb6b72e3a258cca69369638f573202f0c
Abdulaziz <a.alluheeb@gmail.com>
2023-07-04 09:35:36 +0300

    clean prev code

--- posts_app/views.py
+++ posts_app/views.py
@@ -1,8 +1,3 @@
-import datetime
 from django.shortcuts import render
 
 # Create your views here.
-from .tasks import my_scheduled_task
-def register_task():
-    # Schedule the task to run every day at 8:00 AM
-    my_scheduled_task.schedule(repeat=Task.DAILY, time=datetime.time(hour=8, minute=0))
